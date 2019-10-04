02ddd593ec35d5e9afaced0dafee10dc76d98ff0
vivekgondaliya <vivekgondaliyaatwork@gmail.com>
2019-10-04 09:44:39 +0200

    successfully create first note via API call from command line

--- create.js
+++ create.js
@@ -1,4 +1,4 @@
-import uuid from "uuid"; 
+import uuid from "uuid";
 import AWS from "aws-sdk";
 
 //AWS.config.update({ region: "my-region" });
@@ -9,9 +9,9 @@ export function main(event, context, callback) {
     const data = JSON.parse(event.body);
     const params = {
         TableName: "notes",
-        // 'Item' contains the attributes of the item to be created 
-        // - 'userId': user identities are federated through the 
-            //Cognito Identity Pool, we will use the identity id as the user id of the authenticated user 
+        // 'Item' contains the attributes of the item to be created
+        // - 'userId': user identities are federated through the
+        //Cognito Identity Pool, we will use the identity id as the user id of the authenticated user
         // - 'noteId': a unique uuid
         // - 'content': parsed from request body
         // - 'attachment': parsed from request body
@@ -22,35 +22,37 @@ export function main(event, context, callback) {
             content: data.content,
             attachment: data.attachment,
             createdAt: Date.now()
-            }
+        }
     };
 
     dynamoDb.put(params, (error, data) => {
         // Set response headers to enable CORS (Cross-Origin Resource Sharing)
-        const headers = { 
+        const headers = {
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Credentials": true
         };
-    
+
         // Return status code 500 on error
         if (error) {
             const response = {
                 statusCode: 500,
                 headers: headers,
-                body: JSON.stringify({ status: false })
+                body: JSON.stringify({
+                    status: false
+                })
             };
 
-            callback(null, response); 
+            callback(null, response);
             return;
         }
-    
+
         // Return status code 200 and the newly created item
         const response = {
             statusCode: 200,
             headers: headers,
             body: JSON.stringify(params.Item)
         };
-    
-        callback(null, response); 
+
+        callback(null, response);
     });
- }
+}
